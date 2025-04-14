3d7478fabc631fa2838f5e692af52cabbb743bb0
Wefky Hamed <wefky.hamed@outlook.com>
2025-04-14 15:59:54 +0300

    Update hero

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,14 +7,6 @@ import Navbar from './components/Navbar';
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col bg-[#1e2328]">
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{
-          backgroundImage: 'url(/images/Lönnrot_HeroImage.jpg)',
-        }}
-      >
-        <div className="absolute inset-0 bg-black opacity-85"></div>
-      </div>
       <div className="relative z-10 container mt-24 mx-auto px-12 py-4">
         <Hero />
         <Navbar />
