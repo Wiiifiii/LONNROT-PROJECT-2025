051907ef788e2035c4f8de239d8b6bda79785775
Wefky Hamed <wefky.hamed@outlook.com>
2025-04-19 23:48:53 +0300

    Remove obsolete Prisma migration files

--- prisma/migrations/20250204192428_remove_autoincrement/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "books" ALTER COLUMN "id" DROP DEFAULT;
-DROP SEQUENCE "books_id_seq";
--- prisma/migrations/20250208190201_add_autoincrement_to_book_id/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE books_id_seq;
-ALTER TABLE "books" ALTER COLUMN "id" SET DEFAULT nextval('books_id_seq');
-ALTER SEQUENCE books_id_seq OWNED BY "books"."id";
