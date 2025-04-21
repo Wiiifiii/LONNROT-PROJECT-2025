60713179fd926aba9f6aee4c28f2577fa1f3964e
Wefky Hamed <wefky.hamed@outlook.com>
2025-04-21 11:33:14 +0300

    fix: Correct import of useSession and signOut from next-auth/react

--- src/app/profile/tabs/DangerTab.jsx
+++ src/app/profile/tabs/DangerTab.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-import { signOut } from "next-auth/react";
+import { signOut } from "next-auth/react"; // Correct import for useSession
 import Button from "@/app/components/Button";
 
 export default function DangerTab() {
