79785eee605796d4b8db017d239d7a2ab0a5fdb8
Wefky Hamed <wefky.hamed@outlook.com>
2025-04-21 09:28:34 +0300

    update: providers.jsx

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,9 +1,13 @@
+// src/app/providers.jsx
 "use client";
-import React from "react";
-import { SessionProvider } from "next-auth/react";
 
+import React from "react";
+import { SessionProvider } from "next-auth/react"; // Import SessionProvider from NextAuth
+
+// The Providers component wraps your children in the SessionProvider to manage authentication state
 export function Providers({ children }) {
   return (
+    // SessionProvider manages the session globally, keeping track of the logged-in state
     <SessionProvider>
       {children}
     </SessionProvider>
