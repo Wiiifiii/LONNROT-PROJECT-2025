6f8bcd7c81578491827a72e36dc3f219a9f0cf85
Wefky Hamed <wefky.hamed@outlook.com>
2025-05-16 10:23:07 +0300

    Open the community page for testing

--- src/app/community/page.jsx
+++ src/app/community/page.jsx
@@ -1,25 +1,25 @@
-// // app/community/page.jsx
-// import ClientCommunity      from "@/app/components/Community/ClientCommunity.client";
-// import NotificationProvider from "@/app/components/Layout/NotificationProvider.client";
-// import { getServerSession } from "next-auth";
-// import { authOptions }      from "@/lib/authOptions";
-// import Link               from 'next/link'
+// app/community/page.jsx
+import ClientCommunity      from "@/app/components/Community/ClientCommunity.client";
+import NotificationProvider from "@/app/components/Layout/NotificationProvider.client";
+import { getServerSession } from "next-auth";
+import { authOptions }      from "@/lib/authOptions";
+import Link               from 'next/link'
 
-// export default async function CommunityPage() {
-//   const session = await getServerSession(authOptions);
-//   return (
-//     <NotificationProvider>
-//       <ClientCommunity session={session} />
-//     </NotificationProvider>
-//   );
-// }
-
-
-"use client";
-
-
-import ComingSoon from "@/app/components/Sections/ComingSoon";
-
-export default function FooBarPage() {
-  return <ComingSoon featureName="Foo / Bar" />;
+export default async function CommunityPage() {
+  const session = await getServerSession(authOptions);
+  return (
+    <NotificationProvider>
+      <ClientCommunity session={session} />
+    </NotificationProvider>
+  );
 }
+
+
+// "use client";
+
+
+// import ComingSoon from "@/app/components/Sections/ComingSoon";
+
+// export default function FooBarPage() {
+//   return <ComingSoon featureName="Foo / Bar" />;
+// }
